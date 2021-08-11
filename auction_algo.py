7fc953efe19aa71d313cd499df1f3c24585207a1
Filippo Lenzi <filloax@gmail.com>
2021-08-11 14:46:52 +0200

    gestione task ignorati

--- auction_algo.py
+++ auction_algo.py
@@ -18,20 +18,29 @@ class AuctionAlgorithm:
         self.agent_ids = agent_ids
         self.verbose = verbose
 
+        print("Init with data: \n\tid: {}\n\tbids: {}\n\ttasks: {}\n\tagents: {}\n\tneighbors: {}".format(self.id, self.bids, self.tasks, self.agent_ids, self.agent.in_neighbors))
+
         self.done = False
         self.assigned_tasks = np.zeros(len(self.tasks))
         # ogni agente prova a mantenere aggiornato ciò che sa dei dati circostanti di bid massimi
         # quindi mantiene l'intera matrice (una riga per agente) da aggiornare all'evenienza
+
         self.max_bids = np.zeros((len(self.agent_ids), len(self.tasks)))
         self.selected_task = -1
 
         self.prev_max_bids = np.array([-1 for _ in range(len(self.tasks))])
         self.max_bids_equal_cnt = 0
 
-    def auction_phase(self):
+    def auction_phase(self, iter_num="."):
         # Sommatoria della riga corrispondente all'agente i
         if (sum(self.assigned_tasks) == 0):
-            valid_tasks = list(filter(lambda task: self.bids[task] >= self.max_bids[self.id][task], self.tasks))
+            # Task ottenibili solo quelli dove il bid è maggiore del massimo pre-esistente,
+            # ignorando bid nulli quindi non ancora assegnati (se altrui)
+            # oppure non interessati (se propri)
+            # Tutto questo per gestire bid negativi (per trovare il minimo)
+            valid_tasks = list(
+                filter(lambda task: (self.bids[task] >= self.max_bids[self.id][task] or self.max_bids[self.id][task] == 0) and self.bids[task] != 0, 
+                self.tasks))
 
             # print(self.id, self, valid_tasks)
             if len(valid_tasks) > 0:
@@ -41,27 +50,39 @@ class AuctionAlgorithm:
                 self.selected_task = list(filter(lambda task: self.bids[task] == max_valid_bid, valid_tasks))[0]
                 
                 if self.verbose:
-                    print(self.id, "Selected", self.selected_task)
+                    print("{} | {}: Selected {} with bid {}".format(self.id, iter_num, self.selected_task, self.bids[self.selected_task]))
                 self.assigned_tasks[ self.selected_task ] = 1
                 self.max_bids[self.id][self.selected_task] = self.bids[self.selected_task]            
 
-    def converge_phase(self):
+    def converge_phase(self, iter_num="."):
         # Aggiorna la propria lista dei bid massimi in base a quelli ricevuti dagli altri
         # agenti, in modo tale da avere l'effettivo bid globalmente massimo per ogni task
-        self.max_bids[self.id] = [ max( self.max_bids[agent_id][task] for agent_id in self.agent_ids ) for task in self.tasks ]
-        # print(self.id, max_bids[self.id], max_bid_agents)
+        # Come sopra in auction_phase, ignora bid nulli in quanto non ancora assegnati
+        # (altrimenti in caso di bid negativi per trovare il minimo sarebbero sempre il massimo)
+        new_max_bids = [0 for task in self.tasks]
+        for task in self.tasks:
+            task_bids = []
+            for agent_id in self.agent_ids:
+                if self.max_bids[agent_id][task] != 0:
+                    task_bids.append(self.max_bids[agent_id][task])
+            if len(task_bids) > 0:
+                new_max_bids[task] = max(task_bids)
+
+        self.max_bids[self.id] = new_max_bids
 
         # Se il proprio bid per il task selezionato non è quello più alto,
         # allora lascia il task selezionato all'agente che ha posto il bid
         # più alto, ne verrà cercato un'altro all'iterazione successiva
-        max_bid_for_selected_task = max(self.max_bids[agent_id][self.selected_task] for agent_id in self.agent_ids)
-        if max_bid_for_selected_task > self.bids[self.selected_task]:
-            if self.verbose:
-                print(self.id, "Higher bid exists as {}, removing...".format(max_bid_for_selected_task) )
-            self.assigned_tasks[self.selected_task] = 0
+        if self.selected_task >= 0:
+            max_bid_for_selected_task = max(self.max_bids[agent_id][self.selected_task] for agent_id in self.agent_ids if self.max_bids[agent_id][self.selected_task] != 0)
+            if max_bid_for_selected_task > self.bids[self.selected_task]:
+                if self.verbose:
+                    print("{} | {}: Higher bid exists as {}, removing...".format(self.id, iter_num, max_bid_for_selected_task) )
+                self.assigned_tasks[self.selected_task] = 0
+                self.selected_task = -1
         #lprint("- - - - - - - -")
 
-    def check_done(self):
+    def check_done(self, iter_num="."):
         # Controlla terminazione: se la lista max_bids è rimasta
         # invariata per un certo numero di iterazioni (vedi check_done()), e nessun elemento è 0
 
@@ -69,38 +90,59 @@ class AuctionAlgorithm:
         if not (self.max_bids[self.id] == self.prev_max_bids).all():
             self.prev_max_bids = self.max_bids[self.id].copy()
             self.max_bids_equal_cnt = 0
-            if self.verbose and self.verbose >= 2:
-                print(self.id, "Max bids table changed:", self.max_bids[self.id])
+            if self.verbose:
+                print("{} | {}: Max bids table changed: {}".format(self.id, iter_num, self.max_bids[self.id]))
         else:
             self.max_bids_equal_cnt += 1
 
         # Numero di iterazioni senza alterazioni nello stato rilevante per considerare l'operazione finita
         num_stable_runs = 2 * len(self.agent_ids) + 1
-        return np.all((self.max_bids[self.id] != 0)) and np.sum(self.assigned_tasks) == 1 and self.max_bids_equal_cnt >= num_stable_runs
+        # Se tutti i bid massimi sono stati ricevuti, ignorando quelli per i task che questo agente
+        # sta ignorando (vale a dire quelle per cui ha messo bid 0)
+        all_max_bids_set = np.all(np.logical_or(self.max_bids[self.id] != 0, self.bids == 0))
+        return all_max_bids_set and np.sum(self.assigned_tasks) == 1 and self.selected_task >= 0 and self.max_bids_equal_cnt >= num_stable_runs
 
-    def run_iter(self):
-        self.auction_phase()
+    def run_iter(self, iter_num = "."):
+        self.auction_phase(iter_num)
 
         # if self.max_bids_equal_cnt == 0: #se è cambiato dall'ultima iterazione, per evitare invii inutili
         #     #lprint("Sending: {} to {}".format(self.max_bids[self.id], neighbors))
         #     agent.neighbors_send(self.max_bids[self.id])
+        print("pre exchange")
         data = self.agent.neighbors_exchange(self.max_bids[self.id])
+        print("post exchange")
 
         for other_id in filter(lambda id: id != self.id, data):
             self.max_bids[other_id] = data[other_id]
 
-        self.converge_phase()
+        self.converge_phase(iter_num)
 
         #Se tutti elementi sono != 0 e ha un task assegnato, e la lista è rimasta invariata per 2N+1 iterazioni
-        if self.check_done():
+        if self.check_done(iter_num):
             self.agent.neighbors_send(self.max_bids[self.id]) # Evitare hang di altri agenti ancora in attesa
             self.done = True
 
-    def run(self, beforeEach = None):
+    def run(self, beforeEach = None, max_iterations = -1):
+        iterations = 0
         while not self.done:
             if beforeEach != None:
                 beforeEach()
-            self.run_iter()
+            if self.verbose:
+                print("{}: iteration {} started".format(self.id, iterations))
+            self.run_iter(iterations)
+            if self.verbose:
+                print("{}: iteration {} done".format(self.id, iterations))
+            iterations = iterations + 1
+            if max_iterations > 0 and iterations >= max_iterations:
+                print("{}: max iterations reached".format(self.id))
+                self.agent.neighbors_send(self.max_bids[self.id]) # Evitare hang di altri agenti ancora in attesa
+                self.done = True
+
+        if self.verbose:
+            print("{}: Done, selected task {}".format(self.id, self.selected_task))
+
+    def get_result(self):
+        return (self.selected_task, self.assigned_tasks)
 
     def __repr__(self):
         return str(self.__dict__)
